fda35afea55286cd1df99febc6ccbc123c35b7e0
Sushant Gadge <sushigadge11@gmail.com>
2024-07-26 12:53:41 +0530

    made changes

--- initial/src/main/java/com/example/springboot/Application.java
+++ initial/src/main/java/com/example/springboot/Application.java
@@ -6,8 +6,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(Application.class, args);
@@ -20,5 +25,13 @@ public class Application {
 			System.out.println(beanName);
 		}
 	}
+
+	
+		@Override
+		protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+			return application.sources(Application.class);
+		}
 	
 }
+
+
